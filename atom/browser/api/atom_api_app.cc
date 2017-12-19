629ef2b9452baecb611166fdd2d6807384c332a5
Shelley Vohr <shelley.vohr@gmail.com>
2017-12-18 21:16:35 -0500

    first infra things for setLocale

--- atom/browser/api/atom_api_app.cc
+++ atom/browser/api/atom_api_app.cc
@@ -850,6 +850,12 @@ void App::SetDesktopName(const std::string& desktop_name) {
 #endif
 }
 
+// TODO(codebytere)
+void App::SetLocale(std::string locale) {
+  std::string locale = cmd.GetSwitchValueASCII(switches::kLang);
+  return;
+}
+
 std::string App::GetLocale() {
   return l10n_util::GetApplicationLocale("");
 }
@@ -1241,6 +1247,7 @@ void App::BuildPrototype(
       .SetMethod("setPath", &App::SetPath)
       .SetMethod("getPath", &App::GetPath)
       .SetMethod("setDesktopName", &App::SetDesktopName)
+      .SetMethod("setLocale", &App::SetLocale)
       .SetMethod("getLocale", &App::GetLocale)
 #if defined(USE_NSS_CERTS)
       .SetMethod("importCertificate", &App::ImportCertificate)
